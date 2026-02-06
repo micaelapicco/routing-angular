59f5dae953499327ba0abc1d7253a6e125d85023
Micaela Picco <mpicco@ica.com.uy>
2026-02-06 09:59:17 -0300

    Finish routes project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,35 +1,25 @@
 import { Routes } from "@angular/router"
 import { NoTaskComponent } from "./tasks/no-task/no-task.component"
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component"
-import { TasksComponent } from "./tasks/tasks.component"
-import { NewTaskComponent } from "./tasks/new-task/new-task.component"
 import { NotFoundComponent } from "./not-found/not-found.component"
+import { routes as UserRoutes } from '../app/users/users.routes'
 export const routes: Routes = [
         {
             path: '', // <domain>/
-            component: NoTaskComponent
+            component: NoTaskComponent,
+            title: 'No Tasks Available'
         },        
         {
             path: 'users/:userId', // <domain>/users/u1
             component: UserTasksComponent,
-            children: [
-                {
-                    path: '',
-                    redirectTo:'tasks',
-                    pathMatch: 'prefix'
-                },
-                {
-                    path: 'tasks', // <domain>/users/u1/tasks
-                    component: TasksComponent
-                },
-                {
-                    path: 'tasks/new',
-                    component: NewTaskComponent // <domain>/users/u1/tasks/new
-                }
-            ]
+            children: UserRoutes,
+            data: {
+                message: 'User Tasks Page'
+            }
         },
         {
             path: '**', // wildcard route for a 404 page
-            component: NotFoundComponent
+            component: NotFoundComponent,
+            title: 'Page Not Found'
         }
     ]
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,21 +1,41 @@
-import { Component, inject, input, computed } from '@angular/core';
+import { Component, inject, input, computed,OnInit, DestroyRef, signal } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { TasksService } from './tasks.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
-
+export class TasksComponent implements OnInit {
+  sort = signal<'asc' | 'desc'>('desc');
   userId = input.required<string>();
   private  taskService = inject(TasksService)
+  private destroyRef = inject(DestroyRef)
+  private activateRoute = inject(ActivatedRoute);
+  
   userTasks = computed(() => 
-    this.taskService.allTasks().filter(
-      (task) => task.userId === this.userId())
-    );
+    this.taskService
+  .allTasks()
+  .filter((task) => task.userId === this.userId())
+  .sort((a, b) => {
+      if  (this.sort() === 'desc') {
+        return a.id > b.id ? -1 : 1;
+      } else {
+        return a.id > b.id ? 1 : -1;
+      }
+    })
+  );
+
+
+  ngOnInit(): void {
+    const subscription = this.activateRoute.queryParams.subscribe({
+      next: (params) => (this.sort.set(params['sort'])), 
+    })
+    this.destroyRef.onDestroy(() => subscription.unsubscribe())
+  }
 }
